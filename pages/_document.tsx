65ab6abf3897ab24a3a15dee09e6702a80c28a9a
Cbot <aramshiva@icloud.com>
2023-11-07 20:03:31 -0800

    Added Favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,20 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="194x194" href="/favicon/favicon-194x194.png"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicon/site.webmanifest"/>
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+        <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+        <meta name="apple-mobile-web-app-title" content="Hack The Sound"/>
+        <meta name="application-name" content="Hack The Sound"/>
+        <meta name="msapplication-TileColor" content="#2b5797"/>
+        <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png"/>
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <body>
         <Main />
